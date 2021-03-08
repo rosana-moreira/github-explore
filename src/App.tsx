5e142dc2d58fae73ac2f8a21e28dfdac3300502f
rosana-moreira <rooh.moreiraa@gmail.com>
2021-03-08 16:41:31 -0300

    Stilos globais foram criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes /> 
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes /> 
+    </BrowserRouter>
+    <GlobalStyle/>
+  </>
 );
 export default App;
