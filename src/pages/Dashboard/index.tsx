07a5af571dccb75b992bff85e1bfd3437583ccfa
rosana-moreira <rooh.moreiraa@gmail.com>
2021-03-09 18:05:06 -0300

    Conexao com a api do github realizada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
-import {Title} from './styles'
+import React, {useState, FormEvent} from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository{
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+
+}
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>
-  
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void>{
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+  return (
+    <>
+      <img src={logoImg} alt="Github Explore"/>
+      <Title>Explore repositórios no Github</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e)=> setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        {repositories.map(repository=>(
+        <a key={repository.full_name} href="test">
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login}>
+              </img>
+          <div>
+              <strong>{ repository.full_name}</strong>
+              <p>{ repository.description}</p>
+          </div>
+          <FiChevronRight size={20}/>
+          </a>
+          ))}
+      </Repositories>
+    </>
+  );
 };
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,88 @@
 import styled from 'styled-components';
+import{ shade} from 'polished'
 export const Title = styled.h1`
 font-size:48px;
-color:#3A3A3A
+color:#3A3A3A;
+max-width:450px;
+line-height:56px;
+margin-top: 80px;
+`;
+
+export const Form = styled.form`
+margin-top:40px;
+max-width:700px;
+display:flex;
+
+input{
+  flex:1;
+  height:70px;
+  padding: 0px 24px;
+  border:0px;
+  border-radius: 5px 0px 0px 5px;
+  color:#3A3A3A;
+
+  &::placeholder{
+    color: #a8a8b3
+  }
+}
+button{
+  width:210px;
+  height:70px;
+  background-color: #04D361;
+  border-radius: 0px 5px 5px 0px;
+  border: 0px;
+  color:#fff;
+  font-weight:bold;
+  transition: background-color 0.2s;
+
+  &:hover{
+    background: ${shade(0.20, '#04D361')};
+  }
+}
+`;
+export const Repositories = styled.div`
+margin-top:80px;
+max-width:700px;
+a{
+  background:#fff;
+  border-radius:5px;
+  width:100%;
+  padding:24px;
+  display: block;
+  text-decoration:none;
+  display:flex;
+  align-items:center;
+
+  &:hover{
+    transform:translateX(10px);
+    transition:transform 0.2s;
+
+  }
+  &+a{
+    margin-top:16px;
+  }
+}
+img{
+  width:64px;
+  height:64px;
+  border-radius:50%;
+}
+div{
+  margin: 0 16px;
+  flex:1;
+  strong{
+    font-size:20px;
+    color: #3D3D4D;
+  }
+  p{
+    font-size:18px;
+    color:#A8A8B3;
+    margin-top:4px;
+  }
+}
+svg{
+  margin-left:auto;
+  color:#cbcbd6;
+}
 `;
 
